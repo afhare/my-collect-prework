a9911899f71615325e46586357b30387e3e93a14
Alex Hare <alexandra.f.hare@gmail.com>
2019-04-10 12:58:21 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,10 +1,9 @@
 def my_collect (array)
   i=0 
   new_array = []
-  if (array[0].include?(" "))
-    while i<array.length
-    yield new_array << array[i].split(" ").first
-    i+=1
+  while i<array.length
+  yield new_array << array[i].split(" ").first
+  i+=1
     end
     else
     while i<array.length
