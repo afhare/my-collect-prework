618579a79892b796aa9607d9709649c2d8d563c5
Alex Hare <alexandra.f.hare@gmail.com>
2019-04-10 12:58:40 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -2,16 +2,10 @@ def my_collect (array)
   i=0 
   new_array = []
   while i<array.length
-  yield new_array << array[i].split(" ").first
+  yield new_array << array[i]
   i+=1
-    end
-    else
-    while i<array.length
-    yield new_array << array[i].upcase
-    i+=1
-    end
   end
-  array
+   
 end
 
 
